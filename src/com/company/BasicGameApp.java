d59cde2c1c547775bccdd8d7301aeb4e9c6d9f3b
Loc-1 <lachybubble@gmail.com>
2020-05-11 11:49:11 -0600

    collision, sounds and images

--- src/com/company/BasicGameApp.java
+++ src/com/company/BasicGameApp.java
@@ -3,9 +3,12 @@ package com.company;
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
 import com.almasb.fxgl.dsl.FXGL;
+import com.almasb.fxgl.dsl.handlers.CollectibleHandler;
 import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.entity.components.CollidableComponent;
 import com.almasb.fxgl.input.Input;
 import com.almasb.fxgl.input.UserAction;
+import com.almasb.fxgl.physics.CollisionHandler;
 import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -14,28 +17,41 @@ import java.util.Map;
 
 public class BasicGameApp extends GameApplication {
 
+    private Entity player;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public enum EntityType {
+        PLAYER, MONEY
+    }
+
     @Override
     protected void initSettings (GameSettings settings) {
-
         settings.setWidth(600);
         settings.setHeight(600);
         settings.setTitle("Project B");
         settings.setVersion("Θ");
     }
 
-    public static void main(String[] args) {
-	    launch(args);
-    }
-
-    private Entity player;
-
     @Override
     protected void initGame() {
         player = FXGL.entityBuilder()
+                .type(EntityType.PLAYER)
                 .at(300,300)
-                .view(new Rectangle(25,25,Color.YELLOW))
+                .viewWithBBox("bee-player.png")
+                .with(new CollidableComponent(true))
                 .buildAndAttach();
+        player.setScaleX(0.5);
+        player.setScaleY(0.5);
 
+        FXGL.entityBuilder()
+                .type(EntityType.MONEY)
+                .at(500, 200)
+                .viewWithBBox("money.png")
+                .with(new CollidableComponent(true))
+                .buildAndAttach();
     }
 
     @Override
@@ -46,7 +62,6 @@ public class BasicGameApp extends GameApplication {
             @Override
             protected void onAction() {
                 player.translateX(5);
-                FXGL.getWorldProperties().increment("pixelsMoved", +5);
             }
         }, KeyCode.D);
 
@@ -54,7 +69,6 @@ public class BasicGameApp extends GameApplication {
             @Override
             protected void onAction() {
                 player.translateX(-5);
-                FXGL.getWorldProperties().increment("pixelsMoved", +5);
             }
         }, KeyCode.A);
 
@@ -62,7 +76,6 @@ public class BasicGameApp extends GameApplication {
             @Override
             protected void onAction() {
                 player.translateY(-5);
-                FXGL.getWorldProperties().increment("pixelsMoved", +5);
             }
         }, KeyCode.W);
 
@@ -70,9 +83,15 @@ public class BasicGameApp extends GameApplication {
             @Override
             protected void onAction() {
                 player.translateY(5);
-                FXGL.getWorldProperties().increment("pixelsMoved", +5);
             }
         }, KeyCode.S);
+
+        input.addAction(new UserAction("Play Sound") {
+            @Override
+            protected void onActionBegin() {
+                FXGL.play("buzz.wav");
+            }
+        }, KeyCode.F);
 
     }
 
@@ -84,6 +103,29 @@ public class BasicGameApp extends GameApplication {
 
         FXGL.getGameScene().addUINode(textPixels);
         textPixels.textProperty().bind(FXGL.getWorldProperties().intProperty("pixelsMoved").asString());
+
+        var honeycomb = FXGL.getAssetLoader().loadTexture("honeycomb.png");
+        honeycomb.setTranslateX(50);
+        honeycomb.setTranslateY(250);
+        honeycomb.setScaleX(0.2);
+        honeycomb.setScaleY(0.2);
+
+        FXGL.getGameScene().addUINode(honeycomb);
+    }
+
+    @Override
+    protected void initPhysics() {
+        FXGL.getPhysicsWorld().addCollisionHandler(new CollisionHandler(EntityType.PLAYER, EntityType.MONEY) {
+            @Override
+            protected void onCollisionBegin(Entity player, Entity money) {
+                FXGL.play("money.wav");
+                FXGL.getWorldProperties().increment("pixelsMoved", +1);
+
+                double randX = Math.random();
+                double randY = Math.random();
+                money.setPosition((randX*566), (randY*566));
+            }
+        });
     }
 
     @Override
